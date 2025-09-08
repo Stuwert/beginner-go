c16a18537a6b25d6995df665fb9e690288556a48
Stuart Urback <stuart@urback.net>
2025-09-07 22:04:56 -0600

    First exposure to returns

--- main.go
+++ main.go
@@ -39,4 +39,50 @@ func main() {
 	var defaultBool bool
 
 	fmt.Printf("Let's learn about some defaults int %d float %f string %s boolean %t\n", defaultInt, defaultFloat, defaultString, defaultBool)
+
+	const pi = 3.14
+
+	const (
+		Monday = 1
+		Tuesday = 2 
+		Wednesday = 3 
+	)
+
+	const constAge int = 54
+
+	fmt.Printf("Monday %d - Tuesday %d Wednesday %d\n", Monday, Tuesday, Wednesday)
+
+
+	// Increments the next integer value for
+	// subsequent declarations
+	const (
+		Jan int = iota + 1
+		Feb
+		Mar
+		Apr
+	)
+
+	fmt.Printf("jan - %d feb - %d mar - %d apr - %d\n", Jan, Feb, Mar, Apr)
+
+	addResult, multiplyResult := add(3, 4)
+
+	fmt.Printf("This is the result %d, %d\n", addResult, multiplyResult)
+}
+
+// Is Exportable
+func Add() {
+
+}
+
+
+// Is Not Exportable
+// The int within the parameters below covers
+// the grounds of all of the parameters leading up to it
+
+// This return value is not a tuple
+// Multiple returns are a first class citizen
+func add(a, b int)(int, int) {
+	// Go can ignore return types
+	// because errors are values 
+	return a+b, a * b
 }
